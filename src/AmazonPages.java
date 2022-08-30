5c83927aab78f9549900576e2308608074d7af49
ovsanna <ovsanna@lionsinserance.com>
2022-08-30 08:47:47 -0700

    AmazonPages changes

--- src/AmazonPages.java
+++ src/AmazonPages.java
@@ -1,11 +1,12 @@
 
 
-public class AmazonPages  {
+public class AmazonPages {
 //changes on line 4
-}
+
 //html for menu
 
 //htlm for search botton.
 
 // html for deals page.
 // html for card page
+}
